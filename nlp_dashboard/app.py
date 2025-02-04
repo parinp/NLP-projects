d597c1303458372a29a7718cf731a9e2dcc5d7d0
Parin Pipake <parinpipake@gmail.com>
2025-02-04 22:47:32 +0000

    Update app.py

--- nlp_dashboard/app.py
+++ nlp_dashboard/app.py
@@ -1,8 +1,10 @@
 import streamlit as st
+import pandas as pd
+import urllib.parse
 from transformers import pipeline
 from wordcloud_visualizer import generate_wordcloud
 from sentiment_analysis import analyze_sentiment
-from news_scraper import fetch_articles_from_url, fetch_and_summarize, summarize_articles, encode_query
+from news_scraper import fetch_articles_from_url, fetch_articles_from_newsapi, fetch_and_summarize, summarize_articles, encode_query
 
 # Streamlit UI
 st.title("Interactive Specific News Summarizer")
@@ -11,23 +13,39 @@ topic = st.text_input("Enter a topic you're interested in:")
 
 if st.button("Get News"):
     if topic.strip():
-        # Fetch URLs from BBC and CNN
-        search_url = encode_query(topic)
-        urls = fetch_articles_from_url(search_url)
-        
-        if urls:
-            st.write("Fetching and summarizing news articles...")
+        articles = fetch_articles_from_newsapi(topic)
+        if articles:
+            st.write("Fetching news articles...")
             
-            # Fetch and summarize articles
-            articles = fetch_and_summarize(urls)
+            # Create a DataFrame for the articles
+            df = pd.DataFrame(articles)
             
+            # Extract the source domain from the URL
+            df['source'] = df['url'].apply(lambda x: urllib.parse.urlparse(x).netloc)
+            
+            # Format the URL as a clickable hyperlink
+            df['url'] = df['url'].apply(lambda x: f'[Link]({x})')
+            
+            # Display the DataFrame in a table
+            st.write("**Articles:**")
+            st.markdown(df[['title', 'source', 'url']].to_markdown(index=False), unsafe_allow_html=True)
+
+            st.write("Summarizing news articles...")
+            
+            # Method 1: Fetch using newspaper4k
+            articles_url = [article["url"] for article in articles]
+            articles_description = fetch_and_summarize(articles_url)
+            
+            # Method 2: Fetch using newsapi description
+            # articles_description = [article["description"] for article in articles]
+
             # Combine summaries into one
-            combined_summary = summarize_articles(articles)
-            
+            combined_summary = summarize_articles(articles_description)
             # Display combined summary
             st.write("**Combined Summary:**")
             st.write(combined_summary)
             
+            st.write("**WordCloud:**")
             # Word Cloud and Sentiment Analysis for the combined text
             wordcloud = generate_wordcloud(combined_summary)
             st.image(wordcloud.to_array())
