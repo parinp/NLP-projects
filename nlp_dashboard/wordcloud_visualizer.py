12d37cd12c42d9de2e9b59937e20939447296b88
Parin Pipake <parinpipake@gmail.com>
2025-02-05 01:48:19 +0000

    Update wordcloud_visualizer.py

--- nlp_dashboard/wordcloud_visualizer.py
+++ nlp_dashboard/wordcloud_visualizer.py
@@ -1,5 +1,4 @@
 from wordcloud import WordCloud
-import matplotlib.pyplot as plt
 
 def generate_wordcloud(text):
     wordcloud = WordCloud(width=800, height=400, background_color='white').generate(text)
